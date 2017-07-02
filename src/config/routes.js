e60fecf7b5d91a32a4bebaf58ab8bf3afc6591f5
Cesarfilho21 <cesar.pc.filho@gmail.com>
2017-07-02 10:25:16 -0300

    authorization done

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
+
+    //Rotas Protegidas
     
-    const router = express.Router()
-    server.use('/api', router)
+    const protecedApi = express.Router()
+    server.use('/api', protecedApi)
+
+    protecedApi.use(auth)
 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protecedApi, '/billingCycles')
+
+    // Rotas Abertas
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
